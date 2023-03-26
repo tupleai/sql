3ef171aaac95565fea024a66cb4d133ebdada35e
demohi <hi@demohi.com>
2023-03-26 16:35:49 +0800

    update prompt

--- src/translateToSQL.js
+++ src/translateToSQL.js
@@ -1,10 +1,8 @@
 import fetch from "isomorphic-unfetch";
 
 const swimmingSchema = `
-Table rDistanceSwimming, columns = [startDate, endDate, value, creationDate]
+Table rDistanceSwimming, columns = [value, creationDate]
 - Column value is a numerical column, Unit is meter
-- Column startDate is a DATETIME type and represents the time when I started swimming. An example of the data in this column is "2022-03-16 12:39:57 +0800".
-- Column endDate is a DATETIME type and represents the time when I finished swimming. An example of the data in this column is "2022-03-16 12:39:57 +0800".
 - Column creationDate is a DATETIME type and represents the time when the swimming record was created. An example of the data in this column is "2022-03-16 12:39:57 +0800".
 - This Table is used to store swimming records where each data record represents one episode of swimming, and a person may swim multiple times in a day.
 `;

e5f95d5447706815ca422f943e35b09b01330e06
demohi <hi@demohi.com>
2023-03-26 15:29:12 +0800

    revert model

--- src/translateToSQL.js
+++ src/translateToSQL.js
@@ -18,7 +18,7 @@ const translateToSQL = async (query, apiKey, tableSchema = "") => {
       max_tokens: 2048,
       n: 1,
       stop: "\\n",
-      model: "code-davinci-002",
+      model: "text-davinci-003",
       frequency_penalty: 0.5,
       presence_penalty: 0.5,
       logprobs: 10,
